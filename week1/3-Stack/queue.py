513f3877c9814c5c48bcdb19da683bec2a1692c4
kakato10 <svetimir.ignatov@gmail.com>
2015-06-10 13:08:05 +0300

    Adding the solutions to the first three tasks

---
+++ week1/3-Stack/queue.py
@@ -0,0 +1,36 @@
+from node import Node
+
+
+class Queue():
+    def __init__(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+
+    def push(self, value):
+        if self.size == 0:
+            self.size = self.size + 1
+            self.head = Node(value)
+        else:
+            node = Node(value)
+            if not self.tail:
+                self.tail = node
+                self.tail.change_previous_node(self.head)
+                self.head.change_next_node(self.tail)
+            else:
+                self.tail.change_next_node(node)
+                node.change_previous_node(self.node)
+                self.tail = node
+            self.size = self.size + 1
+
+    def pop(self):
+        element_to_pop = self.head
+        self.head = self.head.get_next_node()
+        self.size = self.size - 1
+        return element_to_pop
+
+    def get_size(self):
+        return self.size
+
+    def peek(self):
+        return self.head.get_value()
