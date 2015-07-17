944e21f1871867ced3e384da0a06ab4c0e3bdfac
kakato10 <svetimir.ignatov@gmail.com>
2015-07-17 14:58:10 +0300

    Adding proper solution to the first task from week 3

--- week3/1-BST/bst.py
+++ week3/1-BST/bst.py
@@ -1,23 +1,30 @@
 class BST():
     def is_bst(self, array):
-        index = len(array) - 1
-        is_bst = True
-        while index > 0:
-            left_child = array[index - 1]
-            right_child = array[index]
-            parent = array[int((index - 1) / 2)]
-            if left_child:
-                if left_child > parent:
-                    is_bst = False
-                    print(left_child, parent, right_child)
-                    break
-            if right_child:
-                if right_child < parent:
-                    is_bst = False
-                    print(left_child, parent, right_child)
-                    break
-            index -= 2
-        print(is_bst)
+        is_bst = self.check_bst(array, 0, None, None)
+        if(is_bst):
+            print("YES")
+        else:
+            print("NO")
+
+    def check_bst(self, array, subroot_index, min_value, max_value):
+        if len(array) == 0:
+            return True
+        if subroot_index >= len(array):
+            return True
+        value = array[subroot_index]
+        if value == 0:
+            return True
+        if min_value:
+            if value < min_value:
+                return False
+        if max_value:
+            if value > max_value:
+                return False
+        left_child = subroot_index * 2 + 1
+        right_child = left_child + 1
+        left_subtree = self.check_bst(array, left_child, min_value, value)
+        right_subtree = self.check_bst(array, right_child, value, max_value)
+        return left_subtree and right_subtree
 
 
 def cast(element):
@@ -28,6 +35,7 @@ def cast(element):
 
 
 def main():
+    size = input()
     array = input().split(" ")
     array = list(map(cast, array))
     bst = BST()
